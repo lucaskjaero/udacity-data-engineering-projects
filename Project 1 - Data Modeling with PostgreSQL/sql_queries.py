30210bd5ca7023a802f119d2c1f6d9218d2a5709
Lucas Kjaero <lucas@lucaskjaero.com>
2019-04-05 23:08:18 -0700

    Schemas for tables created from song_data

--- Project 1 - Data Modeling with PostgreSQL/sql_queries.py
+++ Project 1 - Data Modeling with PostgreSQL/sql_queries.py
@@ -1,10 +1,10 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP table songplays;"
+user_table_drop = "DROP table users;"
+song_table_drop = "DROP table songs;"
+artist_table_drop = "DROP table artists;"
+time_table_drop = "DROP table time;"
 
 # CREATE TABLES
 
@@ -15,9 +15,21 @@ user_table_create = ("""
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+song_id varchar,
+title varchar,
+artist_id varchar,
+year int,
+duration float8);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+artist_id varchar,
+name varchar,
+location varchar,
+lattitude float8,
+longitude float8);
 """)
 
 time_table_create = ("""
