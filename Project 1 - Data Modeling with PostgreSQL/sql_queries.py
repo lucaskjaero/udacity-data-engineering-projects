f911732bbeac5a4fa46e82dc4a9d6a454dc03b1a
Lucas Kjaero <lucas@lucaskjaero.com>
2019-04-17 22:39:23 -0700

    Upsert

--- Project 1 - Data Modeling with PostgreSQL/sql_queries.py
+++ Project 1 - Data Modeling with PostgreSQL/sql_queries.py
@@ -65,24 +65,28 @@ songplay_table_insert = ("""
 INSERT INTO songplays
 (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
 VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 user_table_insert = ("""
 INSERT INTO users
 (user_id, first_name, last_name, gender, level)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 song_table_insert = ("""
 INSERT INTO songs
 (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 artist_table_insert = ("""
 INSERT INTO artists
 (artist_id, name, location, lattitude, longitude)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 
@@ -90,6 +94,7 @@ time_table_insert = ("""
 INSERT INTO TIME
 (start_time, hour, day, week, month, year, weekday)
 VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT DO NOTHING
 """)
 
 # FIND SONGS
