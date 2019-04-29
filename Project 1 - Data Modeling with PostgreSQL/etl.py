81016f1206ea4ad6ff9338a8169e0febec5ea489
Lucas Kjaero <lucas@lucaskjaero.com>
2019-04-28 21:10:06 -0700

    Finish etl

--- Project 1 - Data Modeling with PostgreSQL/etl.py
+++ Project 1 - Data Modeling with PostgreSQL/etl.py
@@ -6,67 +6,60 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
-    try:
-        # open song file
-        df = pd.read_json(filepath, lines=True)
+    # open song file
+    df = pd.read_json(filepath, lines=True)
 
-        # insert song record
-        song_data = (df["song_id"][0], df["title"][0], df["artist_id"]
-                     [0], int(df["year"][0]), float(df["duration"][0]))
-        cur.execute(song_table_insert, song_data)
+    # insert song record
+    song_data = (df["song_id"][0], df["title"][0], df["artist_id"]
+                 [0], int(df["year"][0]), float(df["duration"][0]))
+    cur.execute(song_table_insert, song_data)
 
-        # insert artist record
-        artist_data = (df["artist_id"][0], df["artist_name"][0], df["artist_location"]
-                       [0], float(df["artist_latitude"][0]), float(df["artist_longitude"][0]))
-        cur.execute(artist_table_insert, artist_data)
-    except Exception as e:
-        print("%s caused by %s" % (e, df))
+    # insert artist record
+    artist_data = (df["artist_id"][0], df["artist_name"][0], df["artist_location"]
+                   [0], float(df["artist_latitude"][0]), float(df["artist_longitude"][0]))
+    cur.execute(artist_table_insert, artist_data)
 
 
 def process_log_file(cur, filepath):
-    try:
-        # open log file
-        df = pd.read_json(filepath, lines=True)
+    # open log file
+    df = pd.read_json(filepath, lines=True)
 
-        # filter by NextSong action
-        df = df[df.page == "NextSong"]
+    # filter by NextSong action
+    df = df[df.page == "NextSong"]
 
-        # convert timestamp column to datetime
-        df.ts = pd.to_datetime(df.ts, unit="ms")
+    # convert timestamp column to datetime
+    df.ts = pd.to_datetime(df.ts, unit="ms")
 
-        # insert time data records
-        time_data = (df.ts, df.ts.dt.hour, df.ts.dt.day, df.ts.dt.week,
-                     df.ts.dt.month, df.ts.dt.year, df.ts.dt.weekday)
-        column_labels = ("start_time", "hour", "day", "week", "month", "year", "weekday")
-        time_df = pd.concat(time_data, axis=1)
+    # insert time data records
+    time_data = (df.ts, df.ts.dt.hour, df.ts.dt.day, df.ts.dt.week,
+                 df.ts.dt.month, df.ts.dt.year, df.ts.dt.weekday)
+    column_labels = ("start_time", "hour", "day", "week", "month", "year", "weekday")
+    time_df = pd.concat(time_data, axis=1)
 
-        # Perhaps unneccesary but it helps readability.
-        time_df.columns = column_labels
+    # Perhaps unneccesary but it helps readability.
+    time_df.columns = column_labels
 
-        for i, row in time_df.iterrows():
-            cur.execute(time_table_insert, list(row))
+    for i, row in time_df.iterrows():
+        cur.execute(time_table_insert, list(row))
 
-        # load user table
-        user_df = df[["userId", "firstName", "lastName", "gender", "level"]].drop_duplicates()
+    # load user table
+    user_df = df[["userId", "firstName", "lastName", "gender", "level"]].drop_duplicates()
 
-        # insert user records
-        for i, row in user_df.iterrows():
-            cur.execute(user_table_insert, row)
+    # insert user records
+    for i, row in user_df.iterrows():
+        cur.execute(user_table_insert, row)
 
-        # insert songplay records
-        for index, row in df.iterrows():
+    # insert songplay records
+    for index, row in df.iterrows():
 
-            # get songid and artistid from song and artist tables
-            results = cur.execute(song_select, (row.song, row.artist, row.length))
-            songid, artistid = results if results else None, None
+        # get songid and artistid from song and artist tables
+        results = cur.execute(song_select, (row.song, row.artist, row.length))
+        songid, artistid = results if results else None, None
 
-            # insert songplay record
-            songplay_data = (row["ts"], row["userId"], row["level"], songid,
-                             artistid, row["sessionId"], row["location"], row["userAgent"])
-            cur.execute(songplay_table_insert, songplay_data)
-
-    except Exception as e:
-        print("%s caused by %s" % (e, df))
+        # insert songplay record
+        songplay_data = (row["ts"], row["userId"], row["level"], songid,
+                         artistid, row["sessionId"], row["location"], row["userAgent"])
+        cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
